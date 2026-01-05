ecbfca1def5ab8df762cf4b161c69b578c58dfc4
Diegoodlvv <190013865+Diegoodlvv@users.noreply.github.com>
2026-01-05 11:50:47 +0100

    création de l'entité CoursePeriod

--- src/Entity/SchoolYear.php
+++ src/Entity/SchoolYear.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SchoolYearRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -33,6 +35,17 @@ class SchoolYear
     #[Assert\NotNull()]
     #[Assert\NotBlank()]
     private ?\DateTime $end_date = null;
+
+    /**
+     * @var Collection<int, CoursePeriod>
+     */
+    #[ORM\OneToMany(targetEntity: CoursePeriod::class, mappedBy: 'school_year_id')]
+    private Collection $coursePeriods;
+
+    public function __construct()
+    {
+        $this->coursePeriods = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -71,6 +84,36 @@ class SchoolYear
     public function setEndDate(\DateTime $end_date): static
     {
         $this->end_date = $end_date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CoursePeriod>
+     */
+    public function getCoursePeriods(): Collection
+    {
+        return $this->coursePeriods;
+    }
+
+    public function addCoursePeriod(CoursePeriod $coursePeriod): static
+    {
+        if (!$this->coursePeriods->contains($coursePeriod)) {
+            $this->coursePeriods->add($coursePeriod);
+            $coursePeriod->setSchoolYearId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCoursePeriod(CoursePeriod $coursePeriod): static
+    {
+        if ($this->coursePeriods->removeElement($coursePeriod)) {
+            // set the owning side to null (unless already changed)
+            if ($coursePeriod->getSchoolYearId() === $this) {
+                $coursePeriod->setSchoolYearId(null);
+            }
+        }
 
         return $this;
     }
