ab027a85f893070c08bee4dd3a3770edf1bbaef1
Diegoodlvv <190013865+Diegoodlvv@users.noreply.github.com>
2026-01-05 09:46:51 +0100

    ajout de la dépendance pour l'authentification

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
