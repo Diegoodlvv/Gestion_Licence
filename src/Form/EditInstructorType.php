7980897ca1dcf1c5e6dc06c4f51fe2f93063ebcb
ANDRE Antoine <151397736+toto-elgringo@users.noreply.github.com>
2026-01-17 15:08:22 +0100

    formulaire de modification d'un instructor
    je me suis trompe en lisant les issues et j'ai fait une issue a la place d'une autre donc le commit ne correspond pas du tout a la branch

--- src/Form/EditInstructorType.php
+++ src/Form/EditInstructorType.php
@@ -7,38 +7,31 @@ use App\Entity\Module;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NewInstructorType extends AbstractType
+class EditInstructorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email', EmailType::class, [
-                'mapped' => false,
+                'property_path' => 'user.email',
                 'label' => 'Email',
                 'attr' => ['class' => 'w-full px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring focus:ring-black focus:border-black', 'placeholder' => 'exemple@email.com'],
                 'label_attr' => ['class' => 'block text-sm text-slate-700 mb-1'],
             ])
             ->add('firstname', TextType::class, [
-                'mapped' => false,
+                'property_path' => 'user.firstname',
                 'label' => 'Prénom',
                 'attr' => ['class' => 'w-full px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring focus:ring-black focus:border-black'],
                 'label_attr' => ['class' => 'block text-sm text-slate-700 mb-1'],
             ])
             ->add('lastname', TextType::class, [
-                'mapped' => false,
+                'property_path' => 'user.lastname',
                 'label' => 'Nom',
                 'attr' => ['class' => 'w-full px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring focus:ring-black focus:border-black'],
-                'label_attr' => ['class' => 'block text-sm text-slate-700 mb-1'],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                'mapped' => false,
-                'label' => 'Mot de passe',
-                'attr' => ['autocomplete' => 'new-password', 'class' => 'w-full px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring focus:ring-black focus:border-black'],
                 'label_attr' => ['class' => 'block text-sm text-slate-700 mb-1'],
             ])
             ->add('module', EntityType::class, [
