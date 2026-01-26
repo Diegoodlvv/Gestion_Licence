b2045fdd926a9b0afdb8a5cb2e079d8c6bffa57b
ANDRE Antoine <151397736+toto-elgringo@users.noreply.github.com>
2026-01-26 10:18:27 +0100

    ajout lecture des bloc enseignant + filtre

--- src/Controller/TeachingBlockController.php
+++ src/Controller/TeachingBlockController.php
@@ -5,14 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\TeachingBlockFilterType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\TeachingBlockRepository;
+use Knp\Component\Pager\PaginatorInterface;
 
 final class TeachingBlockController extends AbstractController
 {
     #[Route('/teachingBlock', name: 'app_teaching_block')]
-    public function index(): Response
+    public function index(Request $request, TeachingBlockRepository $teachingBlockRepository, PaginatorInterface $pagination): Response
     {
+        $form = $this->createForm(TeachingBlockFilterType::class);
+        $form->handleRequest($request);
+
+        $teachingBlock = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $code = $form->get('code')->getData();
+            $name = $form->get('name')->getData();
+
+            if ($code || $name) {
+                $teachingBlock = $teachingBlockRepository->teachingBlockByFilters($code, $name);
+            } else {
+                $teachingBlock = $teachingBlockRepository->findAll();
+            }
+        } else {
+            $teachingBlock = $teachingBlockRepository->findAll();
+        }
+
+        $pagination = $pagination->paginate(
+            $teachingBlock,
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('teaching_block/index.html.twig', [
-            'controller_name' => 'TeachingBlockController',
+            'teachingBlock' => $pagination,
+            'form' => $form,
         ]);
     }
 }
