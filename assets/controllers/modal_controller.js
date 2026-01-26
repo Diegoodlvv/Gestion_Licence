7e2c44abdb51e4412946a72fb35fa11e7efad8e8
ANDRE Antoine <151397736+toto-elgringo@users.noreply.github.com>
2026-01-26 14:50:52 +0100

    debut mise en place de stimulus avec jeff
    Debut de l'imprementation de stimulus pour tout le js

---
+++ assets/controllers/modal_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus";
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    connect() {
+        document.addEventListener("DOMContentLoaded", function () {
+            const deleteButton = document.getElementById("deleteButton");
+            const deletePopUp = document.getElementById("deletePopUp");
+            const closePopUp = document.getElementById("closePopUp");
+
+            // Open
+            deleteButton.addEventListener("click", function () {
+                deletePopUp.classList.remove("hidden");
+                deletePopUp.classList.add("flex");
+            });
+
+            // Close
+            function hidePopUp() {
+                deletePopUp.classList.add("hidden");
+                deletePopUp.classList.remove("flex");
+            }
+            console.log("hello");
+
+            closePopUp.addEventListener("click", hidePopUp);
+
+            // Close clique côté
+            deletePopUp.addEventListener("click", function (e) {
+                if (e.target === deletePopUp) {
+                    hidePopUp();
+                }
+            });
+        });
+    }
+}
