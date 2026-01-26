bfad708ffdbdb923da6b36208671ebcc1c148a52
ANDRE Antoine <151397736+toto-elgringo@users.noreply.github.com>
2026-01-26 10:23:48 +0100

    suppression doublon bouton filtre pour filtre instructor

--- src/Form/filter/InstructorFilterType.php
+++ src/Form/filter/InstructorFilterType.php
@@ -31,7 +31,7 @@ class InstructorFilterType extends AbstractType
                     'placeholder' => 'Saisissez le prénom',
                     'class' => 'w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-amber-500 outline-none transition-all'
                 ],
-                 'label_attr' => [
+                'label_attr' => [
                     'class' => 'block text-sm font-medium text-gray-700 mb-1'
                 ]
             ])
@@ -42,12 +42,9 @@ class InstructorFilterType extends AbstractType
                     'placeholder' => 'Saisissez l\'email',
                     'class' => 'w-full px-4 py-2.5 border border-gray-300 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-amber-500 outline-none transition-all'
                 ],
-                 'label_attr' => [
+                'label_attr' => [
                     'class' => 'block text-sm font-medium text-gray-700 mb-1'
                 ]
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Filtrer',
             ])
         ;
     }
